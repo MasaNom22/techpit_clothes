922c74c27b6fc714fdf72b65792b74cabad30ec0
MasaNom22 <masakatsunomura26@gmail.com>
2021-05-31 23:11:51 +0900

    ルート追加

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', 'ProductController@index')->name('product.index');
+Route::name('product.')
+    ->group(function () {
+        Route::get('/', 'ProductController@index')->name('index');
+        Route::get('/product/{id}', 'ProductController@show')->name('show');
+    });
+
+Route::name('line_item.')
+    ->group(function () {
+        Route::post('/line_item/create', 'LineItemController@create')->name('create');
+        Route::post('/line_item/delete', 'LineItemController@delete')->name('delete');
+    });
+
+Route::name('cart.')
+    ->group(function () {
+        Route::get('/cart', 'CartController@index')->name('index');
+        Route::get('/cart/checkout', 'CartController@checkout')->name('checkout');
+        Route::get('/cart/success', 'CartController@success')->name('success');
+    });
