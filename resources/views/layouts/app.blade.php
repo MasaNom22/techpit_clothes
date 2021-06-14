162e12f1fba4c75dd633b1591e2eefea4e3a9c0c
MasaNom22 <masakatsunomura26@gmail.com>
2021-06-14 23:11:06 +0900

    共通ファイル作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,14 @@
     <head>
         <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
         <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     </head>
     <body>
         <nav class="navbar navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a>
-                <a class="fas fa-shopping-cart" href="#"></a>
+                <a class="fas fa-shopping-cart" href="{{ route('cart.index') }}"></a>
             </div>
         </nav>
         @yield('content')
